60711387d9183b6226051b6178d2fd0819718cb9
BJ-Kodehode <bernt25sfj@gmail.com>
2025-04-09 14:43:36 +0200

    small changes

--- oppgave2/src/App.jsx
+++ oppgave2/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
-import { CookieClicker } from "./components/CookieClicker";
-import { CatFacts } from "./components/CatFacts";
-import { Users } from "./components/Users";
+import CookieClicker from "../components/CookieClicker";
+import CatFacts from "../components/CatFacts";
+import Users from "../components/Users";
+
 
 function App() {
   return (
